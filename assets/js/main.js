34b8376e34db5914eb601821c91219be1967eba0
mironhasan <mironmahmud018@gmail.com>
2020-07-09 19:55:16 +0600

    changing chart js

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,49 +28,49 @@ $(function() {
 });
 
 
-// Bar chart
-new Chart(document.getElementById("bar-chart"), {
-    type: 'bar',
-    data: {
-      labels: ["Jan", "Feb", "Mar", "Apr", "May", "jun", "Jul", "Agu", "Sep", "Oct", "Nov", "Dec"],
-      datasets: [
-        {
-          label: "Population (millions)",
-          backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850", "#092381"],
-          data: [2478,5267,734,784,433]
-        }
-      ]
-    },
-    options: {
-      legend: { display: false },
-      title: {
-        display: true,
-        text: 'Predicted world population (millions) in 2050'
-      }
-    }
-});
+// // Bar chart
+// new Chart(document.getElementById("bar-chart"), {
+//     type: 'bar',
+//     data: {
+//       labels: ["Jan", "Feb", "Mar", "Apr", "May", "jun", "Jul", "Agu", "Sep", "Oct", "Nov", "Dec"],
+//       datasets: [
+//         {
+//           label: "Population (millions)",
+//           backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850", "#092381"],
+//           data: [2478,5267,734,784,433]
+//         }
+//       ]
+//     },
+//     options: {
+//       legend: { display: false },
+//       title: {
+//         display: true,
+//         text: 'Predicted world population (millions) in 2050'
+//       }
+//     }
+// });
 
 
-//Doughnut chart
-new Chart(document.getElementById("doughnut-chart"), {
-    type: 'doughnut',
-    data: {
-      labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
-      datasets: [
-        {
-          label: "Population (millions)",
-          backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
-          data: [2478,5267,734,784,433]
-        }
-      ]
-    },
-    options: {
-      title: {
-        display: true,
-        text: 'Predicted world population (millions) in 2050'
-      }
-    }
-});
+// //Doughnut chart
+// new Chart(document.getElementById("doughnut-chart"), {
+//     type: 'doughnut',
+//     data: {
+//       labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+//       datasets: [
+//         {
+//           label: "Population (millions)",
+//           backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+//           data: [2478,5267,734,784,433]
+//         }
+//       ]
+//     },
+//     options: {
+//       title: {
+//         display: true,
+//         text: 'Predicted world population (millions) in 2050'
+//       }
+//     }
+// });
 
 
 
@@ -82,3 +82,298 @@ $(".header__icon").on('click', function(){
   $("#footer__part").animate({paddingLeft : "0px"});
 });
 
+
+
+
+// =================================
+//         REAL TIME CHART
+// =================================
+am4core.ready(function() {
+
+    // Themes begin
+    am4core.useTheme(am4themes_animated);
+    // Themes end
+
+    var chart = am4core.create("realchart", am4charts.XYChart);
+    
+    chart.data = [{
+     "country": "Dec",
+     "visits": 2025
+    }, {
+     "country": "Nov",
+     "visits": 1882
+    }, {
+     "country": "Oct",
+     "visits": 1809
+    }, {
+     "country": "Sep",
+     "visits": 1322
+    }, {
+     "country": "Agu",
+     "visits": 1122
+    }, {
+     "country": "July",
+     "visits": 1114
+    }, {
+     "country": "June",
+     "visits": 984
+    }, {
+     "country": "May",
+     "visits": 711
+    }, {
+     "country": "Apr",
+     "visits": 665
+    }, {
+     "country": "Mar",
+     "visits": 580
+    }, {
+     "country": "Feb",
+     "visits": 443
+    }, {
+     "country": "Jan",
+     "visits": 441
+    }];
+    
+    chart.padding(40, 40, 40, 40);
+    
+    var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+    categoryAxis.renderer.grid.template.location = 0;
+    categoryAxis.dataFields.category = "country";
+    categoryAxis.renderer.minGridDistance = 60;
+    categoryAxis.renderer.inversed = true;
+    categoryAxis.renderer.grid.template.disabled = true;
+    
+    var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+    valueAxis.min = 0;
+    valueAxis.extraMax = 0.1;
+    //valueAxis.rangeChangeEasing = am4core.ease.linear;
+    //valueAxis.rangeChangeDuration = 1500;
+    
+    var series = chart.series.push(new am4charts.ColumnSeries());
+    series.dataFields.categoryX = "country";
+    series.dataFields.valueY = "visits";
+    series.tooltipText = "{valueY.value}"
+    series.columns.template.strokeOpacity = 0;
+    series.columns.template.column.cornerRadiusTopRight = 10;
+    series.columns.template.column.cornerRadiusTopLeft = 10;
+    //series.interpolationDuration = 1500;
+    //series.interpolationEasing = am4core.ease.linear;
+    var labelBullet = series.bullets.push(new am4charts.LabelBullet());
+    labelBullet.label.verticalCenter = "bottom";
+    labelBullet.label.dy = -10;
+    labelBullet.label.text = "{values.valueY.workingValue.formatNumber('#.')}";
+    
+    chart.zoomOutButton.disabled = true;
+    
+    // as by default columns of the same series are of the same color, we add adapter which takes colors from chart.colors color set
+    series.columns.template.adapter.add("fill", function (fill, target) {
+     return chart.colors.getIndex(target.dataItem.index);
+    });
+    
+    setInterval(function () {
+     am4core.array.each(chart.data, function (item) {
+       item.visits += Math.round(Math.random() * 200 - 100);
+       item.visits = Math.abs(item.visits);
+     })
+     chart.invalidateRawData();
+    }, 2000)
+    
+    categoryAxis.sortBySeries = series;
+    
+    }); // end am4core.ready()
+
+
+    
+// =================================
+//         DONUT RADIAL CHART
+// =================================
+    am4core.ready(function() {
+
+        // Themes begin
+        am4core.useTheme(am4themes_animated);
+        // Themes end
+        
+        // Create chart instance
+        var chart = am4core.create("donutchart", am4charts.PieChart);
+        
+        // Add data
+        chart.data = [{
+          "country": "Lithuania",
+          "litres": 501.9
+        }, {
+          "country": "Czech Republic",
+          "litres": 301.9
+        }, {
+          "country": "Ireland",
+          "litres": 201.1
+        }, {
+          "country": "Germany",
+          "litres": 165.8
+        }, {
+          "country": "Australia",
+          "litres": 139.9
+        }, {
+          "country": "Austria",
+          "litres": 128.3
+        }, {
+          "country": "UK",
+          "litres": 99
+        }, {
+          "country": "Belgium",
+          "litres": 60
+        }, {
+          "country": "The Netherlands",
+          "litres": 50
+        }];
+        
+        // Add and configure Series
+        var pieSeries = chart.series.push(new am4charts.PieSeries());
+        pieSeries.dataFields.value = "litres";
+        pieSeries.dataFields.category = "country";
+        pieSeries.innerRadius = am4core.percent(50);
+        pieSeries.ticks.template.disabled = true;
+        pieSeries.labels.template.disabled = true;
+        
+        var rgm = new am4core.RadialGradientModifier();
+        rgm.brightnesses.push(-0.8, -0.8, -0.5, 0, - 0.5);
+        pieSeries.slices.template.fillModifier = rgm;
+        pieSeries.slices.template.strokeModifier = rgm;
+        pieSeries.slices.template.strokeOpacity = 0.4;
+        pieSeries.slices.template.strokeWidth = 0;
+
+        chart.legend = new am4charts.Legend();
+        chart.legend.position = "right";
+        
+    }); // end am4core.ready()
+
+
+
+// =================================
+//       BULLETS MOVING BAR
+// =================================
+    am4core.ready(function() {
+
+        // Themes begin
+        am4core.useTheme(am4themes_animated);
+        // Themes end
+        
+        var chart = am4core.create("movingbar", am4charts.XYChart);
+        chart.hiddenState.properties.opacity = 0; // this creates initial fade-in
+        
+        chart.paddingRight = 40;
+
+        chart.data = [{
+            "name": "Monica",
+            "steps": 45688,
+            "href": "https://www.amcharts.com/wp-content/uploads/2019/04/monica.jpg"
+        }, {
+            "name": "Joey",
+            "steps": 35781,
+            "href": "https://www.amcharts.com/wp-content/uploads/2019/04/joey.jpg"
+        }, {
+            "name": "Ross",
+            "steps": 25464,
+            "href": "https://www.amcharts.com/wp-content/uploads/2019/04/ross.jpg"
+        }, {
+            "name": "Phoebe",
+            "steps": 18788,
+            "href": "https://www.amcharts.com/wp-content/uploads/2019/04/phoebe.jpg"
+        }, {
+            "name": "Rachel",
+            "steps": 15465,
+            "href": "https://www.amcharts.com/wp-content/uploads/2019/04/rachel.jpg"
+        }, {
+            "name": "Chandler",
+            "steps": 11561,
+            "href": "https://www.amcharts.com/wp-content/uploads/2019/04/chandler.jpg"
+        }];
+        
+        var categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis());
+        categoryAxis.dataFields.category = "name";
+        categoryAxis.renderer.grid.template.strokeOpacity = 0;
+        categoryAxis.renderer.minGridDistance = 10;
+        categoryAxis.renderer.labels.template.dx = -40;
+        categoryAxis.renderer.minWidth = 120;
+        categoryAxis.renderer.tooltip.dx = -40;
+        
+        var valueAxis = chart.xAxes.push(new am4charts.ValueAxis());
+        valueAxis.renderer.inside = true;
+        valueAxis.renderer.labels.template.fillOpacity = 0.3;
+        valueAxis.renderer.grid.template.strokeOpacity = 0;
+        valueAxis.min = 0;
+        valueAxis.cursorTooltipEnabled = false;
+        valueAxis.renderer.baseGrid.strokeOpacity = 0;
+        valueAxis.renderer.labels.template.dy = 20;
+        
+        var series = chart.series.push(new am4charts.ColumnSeries);
+        series.dataFields.valueX = "steps";
+        series.dataFields.categoryY = "name";
+        series.tooltipText = "{valueX.value}";
+        series.tooltip.pointerOrientation = "vertical";
+        series.tooltip.dy = - 30;
+        series.columnsContainer.zIndex = 100;
+        
+        var columnTemplate = series.columns.template;
+        columnTemplate.height = am4core.percent(50);
+        columnTemplate.maxHeight = 50;
+        columnTemplate.column.cornerRadius(60, 10, 60, 10);
+        columnTemplate.strokeOpacity = 0;
+        
+        series.heatRules.push({ target: columnTemplate, property: "fill", dataField: "valueX", min: am4core.color("#e5dc36"), max: am4core.color("#5faa46") });
+        series.mainContainer.mask = undefined;
+        
+        var cursor = new am4charts.XYCursor();
+        chart.cursor = cursor;
+        cursor.lineX.disabled = true;
+        cursor.lineY.disabled = true;
+        cursor.behavior = "none";
+        
+        var bullet = columnTemplate.createChild(am4charts.CircleBullet);
+        bullet.circle.radius = 20;
+        bullet.valign = "middle";
+        bullet.align = "left";
+        bullet.isMeasured = true;
+        bullet.interactionsEnabled = false;
+        bullet.horizontalCenter = "right";
+        bullet.interactionsEnabled = false;
+        
+        var hoverState = bullet.states.create("hover");
+        var outlineCircle = bullet.createChild(am4core.Circle);
+        outlineCircle.adapter.add("radius", function (radius, target) {
+            var circleBullet = target.parent;
+            return circleBullet.circle.pixelRadius + 8;
+        })
+        
+        var image = bullet.createChild(am4core.Image);
+        image.width = 40;
+        image.height = 40;
+        image.horizontalCenter = "middle";
+        image.verticalCenter = "middle";
+        image.propertyFields.href = "href";
+        
+        image.adapter.add("mask", function (mask, target) {
+            var circleBullet = target.parent;
+            return circleBullet.circle;
+        })
+        
+        var previousBullet;
+        chart.cursor.events.on("cursorpositionchanged", function (event) {
+            var dataItem = series.tooltipDataItem;
+        
+            if (dataItem.column) {
+                var bullet = dataItem.column.children.getIndex(1);
+        
+                if (previousBullet && previousBullet != bullet) {
+                    previousBullet.isHover = false;
+                }
+        
+                if (previousBullet != bullet) {
+                    var hs = bullet.states.getKey("hover");
+                    hs.properties.dx = dataItem.column.pixelWidth;
+                    bullet.isHover = true;
+                    previousBullet = bullet;
+                }
+            }
+        })
+        
+    }); // end am4core.ready()

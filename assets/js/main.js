596916cc7e303788c76492303bd0b9c37e39ef33
mironhasan <mironmahmud018@gmail.com>
2020-07-10 12:05:41 +0600

    Some change and deleted

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,65 +28,20 @@ $(function() {
 });
 
 
-// // Bar chart
-// new Chart(document.getElementById("bar-chart"), {
-//     type: 'bar',
-//     data: {
-//       labels: ["Jan", "Feb", "Mar", "Apr", "May", "jun", "Jul", "Agu", "Sep", "Oct", "Nov", "Dec"],
-//       datasets: [
-//         {
-//           label: "Population (millions)",
-//           backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850", "#092381"],
-//           data: [2478,5267,734,784,433]
-//         }
-//       ]
-//     },
-//     options: {
-//       legend: { display: false },
-//       title: {
-//         display: true,
-//         text: 'Predicted world population (millions) in 2050'
-//       }
-//     }
+
+// // For content full width when click
+// $(".header__icon").on('click', function(){
+//   $("#sidebar__part").animate({left : "-250px"});
+//   $("#header__part").animate({paddingLeft : "0px"});
+//   $("#main__content").animate({paddingLeft : "0px"});
+//   $("#footer__part").animate({paddingLeft : "0px"});
 // });
-
-
-// //Doughnut chart
-// new Chart(document.getElementById("doughnut-chart"), {
-//     type: 'doughnut',
-//     data: {
-//       labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
-//       datasets: [
-//         {
-//           label: "Population (millions)",
-//           backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
-//           data: [2478,5267,734,784,433]
-//         }
-//       ]
-//     },
-//     options: {
-//       title: {
-//         display: true,
-//         text: 'Predicted world population (millions) in 2050'
-//       }
-//     }
-// });
-
-
-
-// For content full width when click
-$(".header__icon").on('click', function(){
-  $("#sidebar__part").animate({left : "-250px"});
-  $("#header__part").animate({paddingLeft : "0px"});
-  $("#main__content").animate({paddingLeft : "0px"});
-  $("#footer__part").animate({paddingLeft : "0px"});
-});
 
 
 
 
 // =================================
-//         REAL TIME CHART
+//      REAL TIME CHART START
 // =================================
 am4core.ready(function() {
 
@@ -182,10 +137,14 @@ am4core.ready(function() {
     
     }); // end am4core.ready()
 
-
-    
 // =================================
-//         DONUT RADIAL CHART
+//      REAL TIME CHART END
+// =================================
+
+
+
+// =================================
+//     DONUT RADIAL CHART START
 // =================================
     am4core.ready(function() {
 
@@ -246,134 +205,91 @@ am4core.ready(function() {
         
     }); // end am4core.ready()
 
+// =================================
+//     DONUT RADIAL CHART END
+// =================================
+
 
 
 // =================================
-//       BULLETS MOVING BAR
+//     COLUMN IMAGE CHART START
 // =================================
     am4core.ready(function() {
 
-        // Themes begin
-        am4core.useTheme(am4themes_animated);
-        // Themes end
-        
-        var chart = am4core.create("movingbar", am4charts.XYChart);
-        chart.hiddenState.properties.opacity = 0; // this creates initial fade-in
-        
-        chart.paddingRight = 40;
-
-        chart.data = [{
-            "name": "Monica",
-            "steps": 45688,
-            "href": "https://www.amcharts.com/wp-content/uploads/2019/04/monica.jpg"
-        }, {
-            "name": "Joey",
-            "steps": 35781,
-            "href": "https://www.amcharts.com/wp-content/uploads/2019/04/joey.jpg"
-        }, {
-            "name": "Ross",
-            "steps": 25464,
-            "href": "https://www.amcharts.com/wp-content/uploads/2019/04/ross.jpg"
-        }, {
-            "name": "Phoebe",
-            "steps": 18788,
-            "href": "https://www.amcharts.com/wp-content/uploads/2019/04/phoebe.jpg"
-        }, {
-            "name": "Rachel",
-            "steps": 15465,
-            "href": "https://www.amcharts.com/wp-content/uploads/2019/04/rachel.jpg"
-        }, {
-            "name": "Chandler",
-            "steps": 11561,
-            "href": "https://www.amcharts.com/wp-content/uploads/2019/04/chandler.jpg"
-        }];
-        
-        var categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis());
-        categoryAxis.dataFields.category = "name";
-        categoryAxis.renderer.grid.template.strokeOpacity = 0;
-        categoryAxis.renderer.minGridDistance = 10;
-        categoryAxis.renderer.labels.template.dx = -40;
-        categoryAxis.renderer.minWidth = 120;
-        categoryAxis.renderer.tooltip.dx = -40;
-        
-        var valueAxis = chart.xAxes.push(new am4charts.ValueAxis());
-        valueAxis.renderer.inside = true;
-        valueAxis.renderer.labels.template.fillOpacity = 0.3;
-        valueAxis.renderer.grid.template.strokeOpacity = 0;
-        valueAxis.min = 0;
-        valueAxis.cursorTooltipEnabled = false;
-        valueAxis.renderer.baseGrid.strokeOpacity = 0;
-        valueAxis.renderer.labels.template.dy = 20;
-        
-        var series = chart.series.push(new am4charts.ColumnSeries);
-        series.dataFields.valueX = "steps";
-        series.dataFields.categoryY = "name";
-        series.tooltipText = "{valueX.value}";
-        series.tooltip.pointerOrientation = "vertical";
-        series.tooltip.dy = - 30;
-        series.columnsContainer.zIndex = 100;
-        
-        var columnTemplate = series.columns.template;
-        columnTemplate.height = am4core.percent(50);
-        columnTemplate.maxHeight = 50;
-        columnTemplate.column.cornerRadius(60, 10, 60, 10);
-        columnTemplate.strokeOpacity = 0;
-        
-        series.heatRules.push({ target: columnTemplate, property: "fill", dataField: "valueX", min: am4core.color("#e5dc36"), max: am4core.color("#5faa46") });
-        series.mainContainer.mask = undefined;
-        
-        var cursor = new am4charts.XYCursor();
-        chart.cursor = cursor;
-        cursor.lineX.disabled = true;
-        cursor.lineY.disabled = true;
-        cursor.behavior = "none";
-        
-        var bullet = columnTemplate.createChild(am4charts.CircleBullet);
-        bullet.circle.radius = 20;
-        bullet.valign = "middle";
-        bullet.align = "left";
-        bullet.isMeasured = true;
-        bullet.interactionsEnabled = false;
-        bullet.horizontalCenter = "right";
-        bullet.interactionsEnabled = false;
-        
-        var hoverState = bullet.states.create("hover");
-        var outlineCircle = bullet.createChild(am4core.Circle);
-        outlineCircle.adapter.add("radius", function (radius, target) {
-            var circleBullet = target.parent;
-            return circleBullet.circle.pixelRadius + 8;
-        })
-        
-        var image = bullet.createChild(am4core.Image);
-        image.width = 40;
-        image.height = 40;
-        image.horizontalCenter = "middle";
-        image.verticalCenter = "middle";
-        image.propertyFields.href = "href";
-        
-        image.adapter.add("mask", function (mask, target) {
-            var circleBullet = target.parent;
-            return circleBullet.circle;
-        })
-        
-        var previousBullet;
-        chart.cursor.events.on("cursorpositionchanged", function (event) {
-            var dataItem = series.tooltipDataItem;
-        
-            if (dataItem.column) {
-                var bullet = dataItem.column.children.getIndex(1);
-        
-                if (previousBullet && previousBullet != bullet) {
-                    previousBullet.isHover = false;
-                }
-        
-                if (previousBullet != bullet) {
-                    var hs = bullet.states.getKey("hover");
-                    hs.properties.dx = dataItem.column.pixelWidth;
-                    bullet.isHover = true;
-                    previousBullet = bullet;
-                }
-            }
-        })
-        
+      // Themes begin
+      am4core.useTheme(am4themes_animated);
+      // Themes end
+      
+      // Create chart instance
+      var chart = am4core.create("columnchart", am4charts.XYChart);
+      
+      // Add data
+      chart.data = [{
+          "name": "John",
+          "points": 35654,
+          "color": chart.colors.next(),
+          "bullet": "https://www.amcharts.com/lib/images/faces/A04.png"
+      }, {
+          "name": "Damon",
+          "points": 65456,
+          "color": chart.colors.next(),
+          "bullet": "https://www.amcharts.com/lib/images/faces/C02.png"
+      }, {
+          "name": "Patrick",
+          "points": 45724,
+          "color": chart.colors.next(),
+          "bullet": "https://www.amcharts.com/lib/images/faces/D02.png"
+      }, {
+          "name": "Mark",
+          "points": 13654,
+          "color": chart.colors.next(),
+          "bullet": "https://www.amcharts.com/lib/images/faces/E01.png"
+      }];
+      
+      // Create axes
+      var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+      categoryAxis.dataFields.category = "name";
+      categoryAxis.renderer.grid.template.disabled = true;
+      categoryAxis.renderer.minGridDistance = 30;
+      categoryAxis.renderer.inside = true;
+      categoryAxis.renderer.labels.template.fill = am4core.color("#fff");
+      categoryAxis.renderer.labels.template.fontSize = 20;
+      
+      var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+      valueAxis.renderer.grid.template.strokeDasharray = "4,4";
+      valueAxis.renderer.labels.template.disabled = true;
+      valueAxis.min = 0;
+      
+      // Do not crop bullets
+      chart.maskBullets = false;
+      
+      // Remove padding
+      chart.paddingBottom = 0;
+      
+      // Create series
+      var series = chart.series.push(new am4charts.ColumnSeries());
+      series.dataFields.valueY = "points";
+      series.dataFields.categoryX = "name";
+      series.columns.template.propertyFields.fill = "color";
+      series.columns.template.propertyFields.stroke = "color";
+      series.columns.template.column.cornerRadiusTopLeft = 15;
+      series.columns.template.column.cornerRadiusTopRight = 15;
+      series.columns.template.tooltipText = "{categoryX}: [bold]{valueY}[/b]";
+      
+      // Add bullets
+      var bullet = series.bullets.push(new am4charts.Bullet());
+      var image = bullet.createChild(am4core.Image);
+      image.horizontalCenter = "middle";
+      image.verticalCenter = "bottom";
+      image.dy = 20;
+      image.y = am4core.percent(100);
+      image.propertyFields.href = "bullet";
+      image.tooltipText = series.columns.template.tooltipText;
+      image.propertyFields.fill = "color";
+      image.filters.push(new am4core.DropShadowFilter());
+      
     }); // end am4core.ready()
+
+    // =================================
+    //     COLUMN IMAGE CHART END
+    // =================================
